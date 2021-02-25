5ef9058fd9064be63988bc42a3001b94a7120dc9
Ysael Pepin <me@ysael.com>
2021-02-25 10:42:17 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar } from "react-native";
+import {DATA} from './data'
 
+
+export const useInvertScrolling = (ref) => {
+  useEffect(() => {
+    console.log('ref', ref)
+    if (!ref.current) {
+      return
+    }
+
+    const scrollNode = ref.current && ref.current.getScrollableNode()
+    if (!scrollNode) {
+      return
+    }
+
+    const listener = scrollNode.addEventListener('wheel', (e) => {
+      scrollNode.scrollTop -= e.deltaY
+      e.preventDefault()
+    })
+    ref.current.setNativeProps({ style: { transform: 'translate3d(0,0,0) scaleY(-1)' } })
+    return () => scrollNode.removeEventListener('wheel', listener)
+  }, [ref.current]) // needs to run any time flatlist mounts
+}
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 function App() {
+
+  const ref = useRef(null)
+  useInvertScrolling(ref)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      ref={ref}
+      inverted
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+  </SafeAreaView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff"
+  },
+  title: {
+    fontSize: 24
+  }
+});
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,334 @@
+export const DATA = [
+    {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    },
+     {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion RingsOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion Ring", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["French Fries", "Onion RingsOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion Ring", "Fried Shrimps"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["French Fries", "Onion RingsOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion Ring", "Fried Shrimps"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps","French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }, {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["French Fries", "Onion RingsOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion RingOnion Ring", "Fried Shrimps"]
+    }
+  ];
